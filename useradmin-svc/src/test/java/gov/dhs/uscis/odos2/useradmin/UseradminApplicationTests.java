5673e8ca95f9d635a5ebebafe01e0f10d4de94cb
kkrishnan <karthik.krishnan@gsa.gov>
2018-04-16 10:44:11 -0400

    Useradmin service inital commit

---
+++ useradmin-svc/src/test/java/gov/dhs/uscis/odos2/useradmin/UseradminApplicationTests.java
@@ -0,0 +1,16 @@
+package gov.dhs.uscis.odos2.useradmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UseradminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
