9393abdf834650cbb17c66e190f910becd450676
tuxotron <rafael@troncoso.org>
2018-04-18 10:24:37 -0400

    added test profile

--- registration-svc/src/test/java/gov/dhs/uscis/odos2/reservation/service/ReservationCreationTest.java
+++ registration-svc/src/test/java/gov/dhs/uscis/odos2/reservation/service/ReservationCreationTest.java
@@ -9,9 +9,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.DayOfWeek;
@@ -26,6 +28,7 @@ import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class ReservationCreationTest {
 
     @Autowired
--- registration-svc/src/test/java/gov/dhs/uscis/odos2/reservation/util/ReservationConflictHelperTest.java
+++ registration-svc/src/test/java/gov/dhs/uscis/odos2/reservation/util/ReservationConflictHelperTest.java
@@ -7,8 +7,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalTime;
@@ -18,6 +20,7 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class ReservationConflictHelperTest {
 
 
