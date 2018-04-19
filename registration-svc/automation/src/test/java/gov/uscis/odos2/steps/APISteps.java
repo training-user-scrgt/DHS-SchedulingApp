6e4eaa15a7c421e7a3a2acb7a83148b0cdcb0a86
ricardo.mediavilla <ricardo.mediavilla@karsun-llc.com>
2018-04-19 17:05:11 -0400

    Test Automation Update
    Updated happy path test automation case. Test needs to better handle
    case where database has reservation on all possibly test dates.

--- registration-svc/automation/src/test/java/gov/uscis/odos2/steps/APISteps.java
+++ registration-svc/automation/src/test/java/gov/uscis/odos2/steps/APISteps.java
@@ -4,8 +4,6 @@ import static io.restassured.RestAssured.given;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-
 import org.junit.Assert;
 import org.springframework.stereotype.Component;
 
@@ -25,29 +23,31 @@ import io.restassured.response.Response;
 @Component
 public class APISteps extends Steps {
 
-	@Given("^I make a reservation for today$")
+	@Given("^I make a reservation$")
 	public void i_make_a_reservation_for_today() {
 		RestAssured.baseURI = LoadProperties.getProperty("reservation.api.url");
 
 		LocalDate today = LocalDate.now();
-		String formattedDate = today.format(DateTimeFormatter.ISO_DATE);
+		LocalDate weekDay = getWeekDay(today);
 		
-		// Save current reservation counts
-		int reservationCount = getReservationCountForDate(formattedDate);
-		executionContext.getCurrentScenarioObj().addProperty("preTestReservationCount", reservationCount);
-
+		// Get date where there's no reservations on the selected date
+		String formattedWeekDay = getDayWithNoReservations(weekDay);
+		
+		// We made sure there are no reservations on the selected date
+		executionContext.getCurrentScenarioObj().addProperty("preTestReservationCount", 0);
+		
 		JsonObject reservationBody = new JsonObject();
-		reservationBody.addProperty("reservationDate", formattedDate);
-		reservationBody.addProperty("startTime", "09:00");
-		reservationBody.addProperty("endTime", "10:00");
-		reservationBody.addProperty("conferenceTitle", "Automation_" + formattedDate);
+		reservationBody.addProperty("reservationDate", formattedWeekDay);
+		reservationBody.addProperty("startTime", "08:00");
+		reservationBody.addProperty("endTime", "09:00");
+		reservationBody.addProperty("conferenceTitle", "Automation_" + formattedWeekDay);
 		reservationBody.addProperty("roomId", "1");
 
 		given().body(reservationBody).contentType(ContentType.JSON).post().then().statusCode(201)
 				.extract().response();
 		// Note: if status is not 201, step will fail and so will scenario
 
-		executionContext.getCurrentScenarioObj().addProperty("reservationDate", formattedDate);
+		executionContext.getCurrentScenarioObj().addProperty("reservationDate", formattedWeekDay);
 	}
 
 	@Then("^my reservation can be retrieved$")
@@ -83,9 +83,70 @@ public class APISteps extends Steps {
 		return parser.parse(jsonString);
 	}
 	
+	/**
+	 * Reservation count for a given date
+	 * 
+	 * @param formattedDate e.g. 2018-04-20
+	 * @return
+	 */
 	private int getReservationCountForDate(String formattedDate) {
 		Response response = given().contentType(ContentType.JSON).pathParam("localDate", formattedDate).when()
 				.get("/{localDate}").then().statusCode(200).extract().response();
 		return response.jsonPath().getList("id").size();
+	}
+	
+	/**
+	 * Get a day of the week that avoids Sat and Sun
+	 * 
+	 * @param localDate
+	 * @return
+	 */
+	private LocalDate getWeekDay(LocalDate localDate) {
+		LocalDate newLocalDate = localDate.plusDays(1);
+		
+		switch (newLocalDate.getDayOfWeek()) {		
+		case SATURDAY:
+			newLocalDate = newLocalDate.plusDays(2);
+			break;
+		case SUNDAY:
+			newLocalDate = newLocalDate.plusDays(1);
+			break;
+		default:
+			// nothing to do
+			break;
+		}
+		
+		return newLocalDate;
+	}
+	
+	/**
+	 * Get a day without reservations to avoid issues with
+	 * happy path test.
+	 * 
+	 * TODO Improve test so that it doesn't break in case
+	 * there are no dates without reservations in the system
+	 * 
+	 * @param weekDay
+	 * @return
+	 */
+	private String getDayWithNoReservations(LocalDate weekDay) {
+		int tries = 0;
+		int maxTries = 365;
+		String formattedWeekDay = weekDay.format(DateTimeFormatter.ISO_DATE);
+		while (tries < maxTries) {
+			if (getReservationCountForDate(formattedWeekDay) != 0) {
+				weekDay = getWeekDay(weekDay);
+				formattedWeekDay = weekDay.format(DateTimeFormatter.ISO_DATE);
+			} else {
+				break;
+			}
+			tries++;
+		}
+		
+		if (tries > maxTries) {
+			throw new RuntimeException("Unable to find week date without reservations in database");
+		}
+		
+		return formattedWeekDay;
 	}
 }
