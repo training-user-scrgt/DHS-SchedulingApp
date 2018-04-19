3ed2d7554f785305814b80becb37074acac043aa
tuxotron <rafael@troncoso.org>
2018-04-19 15:14:34 -0400

    created new client with updated code

--- ClientUpdated/src/services/index.ts
+++
@@ -1,4 +0,0 @@
-export { ItemsService } from './items.service';
-export { ProductsService } from './products.service';
-export { AlertsService } from './alerts.service';
-export { InventoryService } from './inventory.service'; 
