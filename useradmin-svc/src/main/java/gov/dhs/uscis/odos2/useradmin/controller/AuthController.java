f27a676968823a48b08e6e928e6a348538fc1d59
tuxotron <rafael@troncoso.org>
2018-04-19 09:21:33 -0400

    Add roles to the claims

--- useradmin-svc/src/main/java/gov/dhs/uscis/odos2/useradmin/controller/AuthController.java
+++ useradmin-svc/src/main/java/gov/dhs/uscis/odos2/useradmin/controller/AuthController.java
@@ -3,6 +3,7 @@ package gov.dhs.uscis.odos2.useradmin.controller;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import gov.dhs.uscis.odos2.useradmin.model.Roles;
 import gov.dhs.uscis.odos2.useradmin.model.Users;
 import gov.dhs.uscis.odos2.useradmin.repository.UsersRepository;
 import org.slf4j.Logger;
@@ -19,16 +20,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.UnsupportedEncodingException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAdjuster;
-import java.time.temporal.TemporalAmount;
-import java.time.temporal.TemporalUnit;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/auth")
@@ -66,9 +62,7 @@ public class AuthController {
             Users user = usersRepository.findByUsername(username);
 
             if (user == null) {
-
-                return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
-
+                return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
             } else {
 
                 Calendar c = Calendar.getInstance();
@@ -76,8 +70,10 @@ public class AuthController {
                 c.add(Calendar.SECOND, 15);
                 Date expirationDate = c.getTime();
 
-                Map<String, Object> headerClaims = new HashMap<String, Object>(1);
+                Map<String, Object> headerClaims = new HashMap<>(1);
                 headerClaims.put("typ", type);
+
+                String roles = user.getRoles().stream().map(Roles::getRole).collect(Collectors.joining(","));
 
                 String token = JWT.create()
                         .withIssuer(issuer)
@@ -88,6 +84,7 @@ public class AuthController {
                         .withClaim("uuid", user.getId().toString())
                         .withClaim("first", user.getFirstName())
                         .withClaim("last", user.getLastName())
+                        .withClaim("roles", roles)
                         .withHeader(headerClaims)
                         .sign(Algorithm.HMAC256(secret));
 
@@ -98,7 +95,6 @@ public class AuthController {
             LOGGER.error(e.getMessage(), e);
             return ResponseEntity.noContent().build();
         }
-
 
 
     }
