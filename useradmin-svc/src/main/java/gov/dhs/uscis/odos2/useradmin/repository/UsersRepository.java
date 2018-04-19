27158eddf42096afd5e78db45c20f982a2fec461
tuxotron <rafael@troncoso.org>
2018-04-19 16:36:38 -0400

    Ignore case on selecting by username

--- useradmin-svc/src/main/java/gov/dhs/uscis/odos2/useradmin/repository/UsersRepository.java
+++ useradmin-svc/src/main/java/gov/dhs/uscis/odos2/useradmin/repository/UsersRepository.java
@@ -1,14 +1,18 @@
 package gov.dhs.uscis.odos2.useradmin.repository;
 
 import gov.dhs.uscis.odos2.useradmin.model.Users;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
 public interface UsersRepository extends CrudRepository<Users, UUID> {
 
-    Users findByUsername (String username);
+    @Query(value = "SELECT * FROM users WHERE LOWER(username) = LOWER(:username)",
+            nativeQuery = true)
+    Users findByUsername (@Param("username") String username);
 
     Users findUsersById (UUID id);
 
