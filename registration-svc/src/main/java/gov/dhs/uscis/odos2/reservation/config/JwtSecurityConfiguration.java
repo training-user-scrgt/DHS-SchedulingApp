850e3919d7facd090eec25bc413e9d267262b614
tuxotron <rafael@troncoso.org>
2018-04-19 15:12:02 -0400

    Add JWT configuration

---
+++ registration-svc/src/main/java/gov/dhs/uscis/odos2/reservation/config/JwtSecurityConfiguration.java
@@ -0,0 +1,47 @@
+package gov.dhs.uscis.odos2.reservation.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Order(1)
+public class JwtSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${jwt.secret}")
+    String secret;
+
+    @Value("${jwt.issuer}")
+    String issuer;
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS);
+        http.addFilterAfter(new JwtFilter(secret, issuer, authenticationManagerBean()),
+                AnonymousAuthenticationFilter.class).antMatcher("/reservation/**");
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+        auth.authenticationProvider(new JwtAuthenticationProvider());
+    }
+}
