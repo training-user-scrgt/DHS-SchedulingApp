f3c2109d71bda7aec9853f1802bdc34447dace0e
tuxotron <rafael@troncoso.org>
2018-04-19 09:20:08 -0400

    Cleanup and changed role name

--- useradmin-svc/src/main/java/gov/dhs/uscis/odos2/useradmin/controller/UserAdminController.java
+++ useradmin-svc/src/main/java/gov/dhs/uscis/odos2/useradmin/controller/UserAdminController.java
@@ -2,18 +2,23 @@ package gov.dhs.uscis.odos2.useradmin.controller;
 
 import gov.dhs.uscis.odos2.useradmin.exception.InvalidUserException;
 import gov.dhs.uscis.odos2.useradmin.exception.UserAlreadyExistsException;
+import gov.dhs.uscis.odos2.useradmin.model.JwtToken;
 import gov.dhs.uscis.odos2.useradmin.model.Users;
 import gov.dhs.uscis.odos2.useradmin.service.UserAdminService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.UUID;
 
@@ -27,8 +32,8 @@ public class UserAdminController {
     private UserAdminService userAdminService;
 
     @GetMapping
-    @PreAuthorize("hasAuthority('Administrator')")
-    public List<Users> getUsers(){
+    @PreAuthorize("hasAuthority('ROLE_REQUESTOR')")
+    public List<Users> getUsers() {
         return userAdminService.findAllUsers();
     }
 
@@ -36,14 +41,15 @@ public class UserAdminController {
     public ResponseEntity<Void> addUser(@RequestBody Users user, UriComponentsBuilder builder) {
         HttpHeaders headers = new HttpHeaders();
         try {
+
             Users newUser = userAdminService.createNewUser(user);
             headers.setLocation(builder.path("/user/{id}").buildAndExpand(newUser.getId()).toUri());
 
         } catch (UserAlreadyExistsException e) {
             LOGGER.error(e.getMessage(), e);
-            return new ResponseEntity<Void>(headers, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(headers, HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
     @PutMapping(value = "/{id}")
@@ -55,23 +61,22 @@ public class UserAdminController {
 
         } catch (InvalidUserException e) {
             LOGGER.error(e.getMessage(), e);
-            return new ResponseEntity<Void>(headers, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(headers, HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<Void>(headers, HttpStatus.OK);
+        return new ResponseEntity<>(headers, HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> removeUser(@PathVariable String id, UriComponentsBuilder builder) {
         HttpHeaders headers = new HttpHeaders();
         try {
-           userAdminService.deleteUser(UUID.fromString(id));
-           //headers.setLocation(builder.path("/users/{id}").buildAndExpand(userID).toUri());
+            userAdminService.deleteUser(UUID.fromString(id));
 
         } catch (InvalidUserException e) {
             LOGGER.error(e.getMessage(), e);
-            return new ResponseEntity<Void>(headers, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(headers, HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<Void>(headers, HttpStatus.OK);
+        return new ResponseEntity<>(headers, HttpStatus.OK);
     }
 
 }
