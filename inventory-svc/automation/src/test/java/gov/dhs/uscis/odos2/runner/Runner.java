2c4f4eefaca1b1a97b8eeae13873df4016113edd
ricardo.mediavilla <ricardo.mediavilla@karsun-llc.com>
2018-04-19 19:36:37 -0400

    Initial commit
    API test automation for inventory service

---
+++ inventory-svc/automation/src/test/java/gov/dhs/uscis/odos2/runner/Runner.java
@@ -0,0 +1,18 @@
+package gov.dhs.uscis.odos2.runner;
+
+import org.junit.runner.RunWith;
+
+import com.karsun.kic.tan.duke.annotations.TestDataFiles;
+import com.karsun.kic.tan.duke.cukes.MergedDataInjectedCucumber;
+
+import cucumber.api.CucumberOptions;
+
+@RunWith(MergedDataInjectedCucumber.class)
+@CucumberOptions(plugin = { "json:build/test-result.json", "html:build/test-results" },
+		tags = {"~@wip"},
+		features = {
+		"src/test/resources/features/"},
+		glue={"com.karsun.kic.tan", "org.openqa", "gov.uscis.odos2"})
+@TestDataFiles(files = { "src/test/resources/data/data.json" })
+public class Runner {
+}
