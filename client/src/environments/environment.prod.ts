982be124da340abaf3e9b189086c95e4965a7a87
Pramod Malhotra <pmalhotra@iiinfo.com>
2018-04-20 00:34:58 -0400

    changes to apiURL

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environment: {production: boolean} = {
+export const environment: {production: boolean, apiURL: string } = {
   production: true,
   apiUrl: 'http://frontend-prod-odos2-res-app.b9ad.pro-us-east-1.openshiftapps.com'
 };
